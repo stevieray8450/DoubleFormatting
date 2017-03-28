d859f851cba412f90a364b099dda244f80fda13e
stevieray8450 <stevieray8450@gmail.com>
2017-03-28 07:50:55 -0500

    first commit -- 3.28 @ 07:50

--- DoubleFormatting/Program.cs
+++ DoubleFormatting/Program.cs
@@ -10,6 +10,23 @@ namespace DoubleFormatting
     {
         static void Main(string[] args)
         {
+            double example1 = 12.159309;
+            double example2 = 1343591.1;
+            double example3 = 0.000003;
+
+            Console.WriteLine(example1.ToString("#.##"));
+            Console.WriteLine(example2.ToString("#.##"));
+            Console.WriteLine(example3.ToString("#.##"));
+
+            decimal decExample1 = 21.3040349m;
+            decimal decExample2 = 12.9999m;
+            int randomInt1 = 43;
+            int randomInt2 = 21;
+
+            Console.WriteLine("{0:C}", decExample1);
+            Console.WriteLine("{0:C}", decExample1+decExample2);
+
+
         }
     }
 }
